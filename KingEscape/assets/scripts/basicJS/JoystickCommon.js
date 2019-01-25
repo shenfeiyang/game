269bfc04ed949b273d4db99ae574d381858b5cd2
shenfeiyang <17858959617@163.com>
2019-01-25 18:03:16 +0800

    更新版本

--- KingEscape/assets/scripts/basicJS/JoystickCommon.js
+++
@@ -1,9 +0,0 @@
-export const TouchType = cc.Enum({
-    DEFAULT: 0,
-    FOLLOW: 1
-})
-export const DirectionType = cc.Enum({
-    FOUR: 4,
-    EIGHT: 8,
-    ALL: 0
-})
--- KingEscape/assets/scripts/sceneJS/sessionData.js
+++
@@ -1,8 +0,0 @@
-class sessionData {
-    buff = [0, 0, 0, 0];
-
-    buff_free = [false, false, false, false];
-
-    isOpen = true;
-}
-export default new sessionData
